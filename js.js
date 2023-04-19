216d3d233d0424f252cb4a4156ac9b9a0b64249e
ahmadnoval <49505609+ahmadnoval@users.noreply.github.com>
2023-04-19 17:39:58 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+function myFunction() {
+	var x = document.getElementById('gambar');
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else	{
+		x.style.display = "none";
+	}
+	var person = {
+  		firstname: "John",
+		lastname: "Doe",
+		age: 50,
+		eyecolor: "blue"
+	};
+}
